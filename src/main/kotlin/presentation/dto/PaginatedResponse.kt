c155ff343c03dc3f92c2ae8c111503d2e5fe6f2a
msyamu <masayabc@gmail.com>
2025-07-09 20:28:21 +0900

    refactor: remove unused data

--- src/main/kotlin/presentation/dto/PaginatedResponse.kt
+++ src/main/kotlin/presentation/dto/PaginatedResponse.kt
@@ -18,14 +18,3 @@ data class PaginationInfo(
     val hasPrevious: Boolean
 )
 
-data class PaginationParams(
-    val page: Int = 1,
-    val limit: Int = 50
-) {
-    val offset: Int get() = (page - 1) * limit
-    
-    init {
-        require(page >= 1) { "Page must be >= 1" }
-        require(limit in 1..100) { "Limit must be between 1 and 100" }
-    }
-}
